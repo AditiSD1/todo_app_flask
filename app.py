1a95482d1f6a3a57bf6bf26ba234f526ad496ad8
AditiSD1 <sisodiyaa841@gmail.com>
2026-06-15 17:47:43 +0530

    switch database to postgreSQL

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 from datetime import datetime
 app = Flask(__name__)
-basedir =os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "todo.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
