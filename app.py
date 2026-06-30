4204692a1afa1710953507b7c8560345b8735cf2
AditiSD1 <sisodiyaa841@gmail.com>
2026-06-30 09:21:57 +0530

    Add REST API endpoints for tasks(GET,POST,PUT,DELETE)

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+from asyncio import Task
 import os
 from flask import Flask, render_template,request,redirect
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 from datetime import datetime,timezone
 app = Flask(__name__)
 
-db_url = os.environ.get('DATABASE_URL')
+db_url = os.environ.get('DATABASE_URL','sqlite:///todo.db')
 if db_url and db_url.startswith('postgres://'):
     db_url = db_url.replace('postgres://', 'postgresql://', 1)
 
@@ -69,5 +71,51 @@ def delete(sno):
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
+
+# GET all tasks
+@app.route('/api/tasks', methods=['GET'])
+def api_get_tasks():
+    tasks = Todo.query.all()
+    return jsonify([
+        {"id": t.id, "title": t.title, "description": t.description, "done": t.done}
+        for t in tasks
+    ])
+
+# GET single task
+@app.route('/api/tasks/<int:task_id>', methods=['GET'])
+def api_get_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    return jsonify({"id": task.id, "title": task.title, "description": task.description, "done": task.done})
+
+# POST - create a task
+@app.route('/api/tasks', methods=['POST'])
+def api_create_task():
+    data = request.get_json()
+    if not data or not data.get('title'):
+        return jsonify({"error": "Title is required"}), 400
+    new_task = Todo(title=data['title'], description=data.get('description', ''))
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({"id": new_task.id, "title": new_task.title}), 201
+
+# PUT - update a task
+@app.route('/api/tasks/<int:task_id>', methods=['PUT'])
+def api_update_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    data = request.get_json()
+    task.title = data.get('title', task.title)
+    task.description = data.get('description', task.description)
+    task.done = data.get('done', task.done)
+    db.session.commit()
+    return jsonify({"message": "Task updated"})
+
+# DELETE
+@app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
+def api_delete_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({"message": "Task deleted"}), 200
+
 if __name__ =="__main__":
     app.run(debug=False) 
